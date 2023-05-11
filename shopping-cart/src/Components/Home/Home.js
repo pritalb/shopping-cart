c410ec7c59029d30ed5fa81a59f018e54e07da33
pritalb <pritalbamnodkar@gmail.com>
2023-05-11 15:52:39 +0530

    added slogan heading to home page

--- shopping-cart/src/Components/Home/Home.js
+++ shopping-cart/src/Components/Home/Home.js
@@ -1,7 +1,11 @@
+import './Home.css';
+
 const Home = () => {
     return (
         <div className="home-page">
-            Home Page!
+            <div className="home-page-heading">
+                Go Anywhere Your Heart Desires
+            </div>
         </div>
     )
 }
--- shopping-cart/src/Components/Home/Home.test.js
+++ shopping-cart/src/Components/Home/Home.test.js
@@ -2,10 +2,27 @@ import {render} from '@testing-library/react';
 import Home from './Home';
 
 describe("Home Component renders correctly", () => {
-    
     test("Home has a Heading", () => {
         const {container} = render(<Home />);
 
-        expect(container.querySelector('.heading')).toBeInTheDocument();
-    })
+        expect(container.querySelector('.home-page-heading')).toBeInTheDocument();
+    });
+
+    test("Home has a Background Image", () => {
+        const {container} = render(<Home />);
+
+        expect(container.querySelector('.page-background')).toBeInTheDocument();
+    });
+
+    test("Home has a Navbar", () => {
+        const {container} = render(<Home />);
+
+        expect(container.querySelector('.navbar')).toBeInTheDocument();
+    });
+
+    test("Home has a Footer", () => {
+        const {container} = render(<Home />);
+
+        expect(container.querySelector('.footer')).toBeInTheDocument();
+    });
 })

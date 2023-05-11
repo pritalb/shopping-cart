873f9c6e2b7111bfdc68a254ad3434f41c92a773
pritalb <pritalbamnodkar@gmail.com>
2023-05-11 16:10:18 +0530

    added background image to home page

--- shopping-cart/src/Components/Home/Home.js
+++ shopping-cart/src/Components/Home/Home.js
@@ -6,6 +6,11 @@ const Home = () => {
             <div className="home-page-heading">
                 Go Anywhere Your Heart Desires
             </div>
+
+            <div className='page-background-container'>
+                <div className='page-background-filter'></div>
+                <img src='./Assets/Images/road.jpg' className='page-background'/>
+            </div>
         </div>
     )
 }

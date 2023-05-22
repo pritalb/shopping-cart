0d4d8fd60e4e59a54c5dcc0c2a9aae87d5a6d77b
pritalb <pritalbamnodkar@gmail.com>
2023-05-22 15:46:37 +0530

    added functionality to next product and previous product buttons on Products Page Component

--- shopping-cart/src/Components/Products/Products.js
+++ shopping-cart/src/Components/Products/Products.js
@@ -10,10 +10,55 @@ const Products = () => {
             name: 'Venom',
             model: 'CCM 2K',
             image: './Assets/Images/car1.jpeg',
-        }
+            price: '2000000',
+        },
+        {
+            name: 'Poison',
+            model: '9CCR Turbo',
+            image: './Assets/Images/car2.jpg',
+            price: '2500000',
+        },
+        {
+            name: 'Define',
+            model: 'Tidal GT 4',
+            image: './Assets/Images/car3.jpg',
+            price: '1500000',
+        },
     ]);
     
     const [currentProductIndex, setCurrentProductIndex] = useState(0);
+
+    const createPageCircles = () => {
+        let arr = [];
+
+        for (let i = 0; i < productsArray.length; i++) {
+            let circleClass = (i === currentProductIndex 
+                                ? 'product-navigation-circle current-circle'
+                                : 'product-navigation-circle')
+            
+            arr.push(<div className={circleClass} ></div>);
+        }
+
+        return arr;
+    }
+
+    const previousProduct = () => {
+        let nextIndex = currentProductIndex - 1;
+        if (nextIndex < 0) {
+            nextIndex = productsArray.length - 1;
+        }
+        
+        setCurrentProductIndex(nextIndex);
+    }
+
+    const nextProduct = () => {
+        let nextIndex = currentProductIndex + 1;
+        if (nextIndex > productsArray.length - 1) {
+            nextIndex = 0;
+        }
+        
+        setCurrentProductIndex(nextIndex);
+    }
 
     return (
         <div className='product-page'>
@@ -35,11 +80,14 @@ const Products = () => {
                 </div>
 
                 <div className='product-navigation'>
-                    <button className='product-navigation-button'> <img className='product-navigation-image previous-button-image' src='./Assets/Images/triangle.png' /> </button>
-                    <div >
-                        <div className='product-navigation-circle'></div>
+                    <button className='product-navigation-button' onClick={previousProduct}> <img className='product-navigation-image previous-button-image' src='./Assets/Images/triangle.png' /> </button>
+                    <div className='circles-container'>
+                        { 
+                        //  <div className='product-navigation-circle'></div>
+                            createPageCircles()
+                        }
                     </div>
-                    <button className='product-navigation-button'> <img className='product-navigation-image next-button-image' src='./Assets/Images/triangle.png' /> </button>
+                    <button className='product-navigation-button' onClick={nextProduct}> <img className='product-navigation-image next-button-image' src='./Assets/Images/triangle.png' /> </button>
                 </div>
             </div>
         </div>

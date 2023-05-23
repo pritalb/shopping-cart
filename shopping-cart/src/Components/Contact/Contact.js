640a80a6f26d459823768baf50b682f0e36154c4
pritalb <pritalbamnodkar@gmail.com>
2023-05-23 16:15:20 +0530

    created contact page component

--- shopping-cart/src/Components/Contact/Contact.js
+++ shopping-cart/src/Components/Contact/Contact.js
@@ -1,7 +1,48 @@
+import './Contact.css';
+import Navbar from '../Navbar/Navbar.js';
+import Footer from '../Footer/Footer.js';
+import PageBackground from '../PageBackground/PageBackground.js';
+
 const ContactPage = () => {
     return (
-        <div>
-            Contact Page
+        <div className='contacts-page'>
+            <Navbar />
+            <PageBackground backgroundPath='./Assets/Images/logobg.png' />
+            <Footer />
+
+            <div className='main-container'>
+                <div className='heading'>
+                    Contact Us
+                </div>
+
+                <div className='contacts-container'>
+                    <div className='contact'>
+                        <div className='contact-heading'> Feedback </div>
+                        <div className='contact-description'> sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt </div>
+                        <div className='contact-buttons-container'>
+                            <button className='contact-button'> Lorem </button>
+                            <button className='contact-button'> Ipsum </button>
+                        </div>
+                    </div>
+
+                    <div className='contact'>
+                        <div className='contact-heading'> Enquiry </div>
+                        <div className='contact-description'> sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt </div>
+                        <div className='contact-buttons-container'>
+                            <button className='contact-button'> Lorem </button>
+                            <button className='contact-button'> Ipsum </button>
+                        </div>
+                    </div>
+
+                    <div className='contact'>
+                        <div className='contact-heading'> Email </div>
+                        <div className='contact-description'> sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt </div>
+                        <div className='contact-email'>
+                            Someemail@mail.com
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
